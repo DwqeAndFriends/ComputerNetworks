46977e71bc554d3a87ef8a81e1e87e45c2c09b3d
Dwqe <45004307+ZPyhagoras@users.noreply.github.com>
2019-04-09 08:07:11 +0800

    Update ByteStuffing.java
    Finish ByteStuffing

--- Exp 2/ByteStuffing.java
+++ Exp 2/ByteStuffing.java
@@ -1,17 +1,16 @@
 public class ByteStuffing {
-    
     private static String PPPByteStuffing(String InfoString1, String flagString, String ESC) {
         StringBuilder newString = new StringBuilder();
-        char[] infoChars = InfoString1.toCharArray(), flagChars = flagString.toCharArray(), escChars = ESC.toCharArray();
+        char[] infoChars = InfoString1.toCharArray();
         for (int i = 0; i < infoChars.length; i++) {
-            if(infoChars[i] == flagChars[0] && infoChars[i + 1] == flagChars[1]) {
-                newString.append(escChars);
-                newString.append(flagChars);
+            if(infoChars[i] == flagString.charAt(0) && infoChars[i + 1] == flagString.charAt(1)) {
+                newString.append(ESC);
+                newString.append(flagString);
                 ++i;
             }
-            else if(infoChars[i] == escChars[0] && infoChars[i + 1] == escChars[1]) {
-                newString.append(escChars);
-                newString.append(escChars);
+            else if(infoChars[i] == ESC.charAt(0) && infoChars[i + 1] == ESC.charAt(1)) {
+                newString.append(ESC);
+                newString.append(ESC);
                 ++i;
             }
             else {
@@ -21,11 +20,44 @@ public class ByteStuffing {
         return flagString + newString.toString() + flagString;
     }
 
+    private static String ByteGetOriginalString(String receivedString, String flagString, String ESC) {
+        char[] receivedChars = receivedString.toCharArray();
+        StringBuilder originalString = new StringBuilder();
+        boolean startFlag = false;
+        for(int i = 0; i < receivedChars.length; i++) {
+            if(receivedChars[i] == ESC.charAt(0) && receivedChars[i + 1] == ESC.charAt(1)) {
+                if(startFlag) {
+                    originalString.append(receivedChars[i + 2]);
+                    originalString.append(receivedChars[i + 3]);
+                }
+                i += 3;
+            }
+            else if(receivedChars[i] == flagString.charAt(0) && receivedChars[i + 1] == flagString.charAt(1)) {
+                if(!startFlag) {
+                    startFlag = true;
+                    ++i;
+                }
+                else {
+                    break;
+                }
+            }
+            else {
+                if(startFlag) {
+                    originalString.append(receivedChars[i]);
+                }
+            }
+        }
+        return originalString.toString();
+    }
+
     public static void main(String[] args) {
         String InfoString1 = "347D7E807E4//0AA7D";
         String FlagString = "7E";
         String ESC = "//";
-        String SendString = PPPByteStuffing(InfoString1, FlagString, ESC);
+        System.out.println(InfoString1);
+        String SendString = "2312//7E676AD" + PPPByteStuffing(InfoString1, FlagString, ESC);
         System.out.println(SendString);
+        String OriginalString = ByteGetOriginalString(SendString, FlagString, ESC);
+        System.out.println(OriginalString);
     }
 }
