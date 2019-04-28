a1543da35e0bc628f3901531ef31aca71942ed07
wojiaozouzixi <39880720+wojiaozouzixi@users.noreply.github.com>
2019-04-28 16:15:35 +0800

    add .ini

--- Exp1/CRC.cpp
+++ Exp1/CRC.cpp
@@ -1,13 +1,14 @@
-// ConsoleApplication1.cpp : �������̨Ӧ�ó������ڵ㡣
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
 #include<iostream>
 #include<string>
-#include<sstream>
 #include<fstream>
 #include<bitset>
 #include<ctime>
+#include<windows.h>
+#include<comutil.h>
+#include<tchar.h>
 using namespace std;
 
 long long int f(string s)//translate a binary string to long
@@ -34,7 +35,7 @@ string t(long long int a, int length)//translate a long to binary
 	return s;
 }
 
-long long GetRemainder(string newString, string GenXString) {
+long long GetRemainder(string newString, string GenXString) {//形成CRC
 	int r = GenXString.length() - 1;
 	string subStr = newString.substr(0, r);
 	long long mod = f(subStr);
@@ -51,7 +52,7 @@ long long GetRemainder(string newString, string GenXString) {
 	return mod;
 }
 
-string GetSendString(string newString, long long mod) {
+string GetSendString(string newString, long long mod) {//验证CRC正确
 	string sendString = newString + t(mod, 16);
 	cout << sendString << endl;
 	return sendString;
@@ -59,37 +60,48 @@ string GetSendString(string newString, long long mod) {
 
 int main()
 {
-	string InfoString1, InfoString2, GenXString;
-	const int CRCLength = 16;//��ֵΪGenXString.length()-1;
+	//从配置文件中读取InfoString1及GenXString 
+	LPTSTR IpPath=new char[MAX_PATH];
+	strcpy(IpPath,".\\CRC.ini");
+	LPTSTR a=new char[100];
+	LPTSTR b=new char[100];
+	GetPrivateProfileString("Strings","InfoString1","",a,100,IpPath);
+	GetPrivateProfileString("Strings","GenXString","",b,100,IpPath);
+	//cout<<a<<endl;
+	//cout<<b<<endl;
+	string InfoString1;
+	string GenXString;
+	InfoString1=(string)(LPCTSTR)a;
+	GenXString=(string)(LPCTSTR)b;
+	
+	
+	string InfoString2;
+	const int CRCLength = 16;//数值为GenXString.length()-1;
 	srand(time(0));
-	InfoString1 = "0110";
-	for (int i = 0; i < 25; i++)
-	{
-		InfoString1 += rand() % 2 + '0';
-	}
-	InfoString1 += "110";
-	GenXString = "10001000000100001";
-	cout << "Send" << endl;
-	cout << InfoString1 << endl;
-	cout << GenXString << endl;
+	//for (int i = 0; i < 25; i++)
+	//{
+	//	InfoString1 += rand() % 2 + '0';
+	//}
+	cout << "CRC-CCITT二进制比特串："<<GenXString << endl<<endl;
+	cout<<"待发送的数据信息二进制比特串："<<InfoString1<<endl;
 	string newString = InfoString1;
 	for (size_t i = 0; i < CRCLength; i++)
 	{
 		newString += "0";
 	}
 	long long mod = GetRemainder(newString, GenXString);
-	cout << (bitset<CRCLength>)mod << endl;
+	cout << "生成循环冗余校验码CRC-Code："<<(bitset<CRCLength>)mod << endl;
 	string SendString = GetSendString(InfoString1, mod);
-	cout << "Receive" << endl;
-	cout << SendString << endl;
-	cout << (bitset<CRCLength>)mod << endl;
+	cout << "带校验和的发送帧："<<SendString << endl<<endl;
+	//cout << (bitset<CRCLength>)mod << endl;
+	cout << "接收数据信息二进制比特串："<<SendString << endl;
 	long long Mod = GetRemainder(SendString, GenXString);
-	cout << Mod << endl;
+	cout << "余数："<<Mod << endl;
 	if (Mod == 0) {
-		cout << "Send Success" << endl;
+		cout << "校验无错" << endl;
 	}
 	else {
-		cout << "Send Fail" << endl;
+		cout << "校验失败" << endl;
 	}
 	system("pause");
 }
