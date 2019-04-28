3bdf053f405b9dbbf94f9395f35f8c8c554bdf86
WTYtemp <49396295+WTYtemp@users.noreply.github.com>
2019-04-28 16:48:30 +0800

    lastest with .ini

--- Exp1/CRC.py
+++ Exp1/CRC.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import math
+import configparser
 
 def GetRemainder(newString, GenXString):
     r = len(GenXString) - 1
@@ -20,14 +21,31 @@ def GetSendString(InfoString1, GenXString):
     for i in range(0, rr):
         newString = newString + "0"
     mod = GetRemainder(newString, GenXString)
+    print("生成循环冗余校验码CRC-Code：",bin(mod).replace("0b",""))
     tmp = int(newString, 2)
     return bin(mod ^ tmp).replace("0b","")
 
 
-InfoString1 = "1101011111"
-GenXString = "10011"
+config = configparser.ConfigParser()
+config.readfp(open('CRC.ini'))
+
+#Sender#
+InfoString1 = config.get("Strings","InfoString1")
+GenXString = config.get("Strings","GenXString")
+print("CRC-CCITT二进制比特串：",GenXString)
+print("\n")
+print("待发送的数据信息二进制比特串：",InfoString1)
 SendString = GetSendString(InfoString1, GenXString)
-print("SendString: ", SendString)
-cmp = GetRemainder(SendString, GenXString)
-print("cmp: ", cmp)
+print("带校验和的发送帧：", SendString)
+print("\n")
+
+#Receiver#
+InfoString2 = config.get("Strings","InfoString2")
+print("接收数据信息二进制比特串：",InfoString2)
+cmp = GetRemainder(InfoString2, GenXString)
+print("余数：",cmp)
+if cmp == 0:
+    print("校验无错")
+else:
+    print("校验有错")
 input("Please enter...")
